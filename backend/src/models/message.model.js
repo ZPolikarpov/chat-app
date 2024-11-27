ff7631c856b2083e7284364b39946824dc432cab
Zakhar Polikarpov <zpolikarpov@veka.de>
2024-11-27 14:04:09 +0100

    Custom Validation that either a text or an image are present

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -22,6 +22,14 @@ const messageSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Custom validation to ensure either text or image is present
+messageSchema.pre('save', function(next) {
+  if (!this.text && !this.image) {
+    next(new Error('Either text or image must be provided'));
+  }
+  next();
+});
+
 const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
